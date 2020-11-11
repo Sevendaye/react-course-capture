18a213429cbf52f50489c19fd1178fbf0269b089
Dominique <Sevendayes@gmail.com>
2020-11-11 13:56:33 +0100

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+//Import Pages
+import AboutUs from './Pages/AboutUs'
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//Page components
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => (
+  <AboutSection />
+);
+
+export default AboutUs;

8c37f4c7870cdb8127be2aa75be2a35a7b39221b
Dominique <Sevendayes@gmail.com>
2020-11-11 20:33:39 +0100

    7. Styled FaqSection

--- src/components/FaqSection/index.js
+++ src/components/FaqSection/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
+import { About } from '../../styles';
 
 const FaqSection = () => (
-  <div className="faq">
+  <Faq>
     <h2>Any Questions <span>FAQ</span></h2>
     <div className="question">
       <h4>How do I start?</h4>
@@ -9,6 +11,7 @@ const FaqSection = () => (
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptate ratione nihil atque reiciendis labore doloribus dolorem incidunt sapiente fugit illo reprehenderit maiores omnis quod, alias deleniti, beatae hic vero!</p>
       </div>
+      <div className="faq-line"></div>
     </div>
     <div className="question">
       <h4>Daily Schedule?</h4>
@@ -16,6 +19,7 @@ const FaqSection = () => (
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptate ratione nihil atque reiciendis labore doloribus dolorem incidunt sapiente fugit illo reprehenderit maiores omnis quod, alias deleniti, beatae hic vero!</p>
       </div>
+      <div className="faq-line"></div>
     </div>
     <div className="question">
       <h4>Different payment method</h4>
@@ -23,6 +27,7 @@ const FaqSection = () => (
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptate ratione nihil atque reiciendis labore doloribus dolorem incidunt sapiente fugit illo reprehenderit maiores omnis quod, alias deleniti, beatae hic vero!</p>
       </div>
+      <div className="faq-line"></div>
     </div>
     <div className="question">
       <h4>What products do you offer.</h4>
@@ -30,8 +35,36 @@ const FaqSection = () => (
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptate ratione nihil atque reiciendis labore doloribus dolorem incidunt sapiente fugit illo reprehenderit maiores omnis quod, alias deleniti, beatae hic vero!</p>
       </div>
+      <div className="faq-line"></div>
     </div>
-  </div>
+  </Faq>
 );
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;

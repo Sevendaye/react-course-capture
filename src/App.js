70c3fd0e23d7693d094ffcebd56dec30cbd00683
Dominique <Sevendayes@gmail.com>
2020-11-11 23:09:05 +0100

    11. Add components MovieDetails

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-//Import Pages
+//Router
+import { Switch, Route } from 'react-router-dom';
+
+//Import Components
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+
+//Import Pages
 import AboutUs from './Pages/AboutUs'
 import ContactUs from './Pages/ContactUs'
 import OurWork from './Pages/OurWork'
-//Router
-import { Switch, Route } from 'react-router-dom';
+import MovieDetails from './Pages/MovieDetails';
 
 function App() {
   return (
@@ -14,7 +18,8 @@ function App() {
       <Nav />
       <Switch>
         <Route exact path='/' component={AboutUs} />
-        <Route path='/work' component={OurWork} />
+        <Route exact path='/work' component={OurWork} />
+        <Route path='/work/:id' component={MovieDetails} />
         <Route path='/contact' component={ContactUs} />
       </Switch>
     </div>

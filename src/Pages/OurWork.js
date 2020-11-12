066e813622734b1b919407d14a577ffaea86d8b4
Dominique <Sevendayes@gmail.com>
2020-11-12 15:36:58 +0100

    15. Add animation OurWork

--- src/Pages/OurWork.js
+++ src/Pages/OurWork.js
@@ -3,7 +3,15 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 //Page animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../animation";
+import { Hide } from "../styles";
 
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
@@ -17,11 +25,19 @@ const OurWork = () => (
     animate="show"
     exit="exit"
   >
+    <motion.div variants={sliderContainer}>
+      <Frame1 variants={slider}></Frame1>
+      <Frame2 variants={slider}></Frame2>
+      <Frame3 variants={slider}></Frame3>
+      <Frame4 variants={slider}></Frame4>
+    </motion.div>
     <Movie>
-      <h2>The Athlete</h2>
-      <div className="line"></div>
+      <motion.h2 variants={fade}>The Athlete</motion.h2>
+      <motion.div variants={lineAnim} className="line"></motion.div>
       <Link to="/work/the-athlete">
-        <img src={athlete} alt="athlete" />
+        <Hide>
+          <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+        </Hide>
       </Link>
     </Movie>
     <Movie>
@@ -54,7 +70,7 @@ const Movie = styled.div`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background: #ccc;
+    background: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -62,6 +78,25 @@ const Movie = styled.div`
     height: 70vh;
     object-fit: cover;
   }
+`;
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  top: 10%;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 export default OurWork;
--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,6 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: {
       duration: 0.5,
     },
@@ -54,4 +53,36 @@ export const photoAnim = {
       duration: 1,
     },
   },
+};
+
+export const lineAnim = {
+  hidden: {
+    width: "0%",
+  },
+  show: {
+    width: "100%",
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+export const slider = {
+  hidden: {
+    x: "-130%",
+    skew: "45deg",
+  },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: {
+      duration: "1",
+      ease: "easeOut",
+    },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15 } },
 };

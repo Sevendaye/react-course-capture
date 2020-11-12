f91a4732c007e32d4767944b86f689aab44b8b59
Dominique <Sevendayes@gmail.com>
2020-11-12 22:28:53 +0100

    20. Add responsive

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -51,5 +51,22 @@ const StyledNav = styled.nav`
     padding-left: 10rem;
     position: relative;
   }
+  @media (max-width: 1200px) {
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 2rem 0rem;
+    #logo {
+      display: inline-block;
+      margin: 1rem;
+    }
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+      li {
+        padding: 0;
+      }
+    }
+  }
 `;
 export default Nav;
--- src/components/useScroll/index.js
+++ src/components/useScroll/index.js
@@ -2,7 +2,7 @@ import { useInView } from "react-intersection-observer";
 import { useAnimation } from "framer-motion";
 
 const useScroll = () => {
-  const [element, controls] = useInView({ threshold: 0.5 });
+  const [element, controls] = useInView({ threshold: 0.3 });
   const animation = useAnimation();
   if (controls) {
     animation.start("show");

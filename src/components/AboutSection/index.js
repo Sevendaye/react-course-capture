2a61588c73f3e403329cc598dc7947a8a09b3b27
Dominique <Sevendayes@gmail.com>
2020-11-12 11:24:56 +0100

    13. Add animation AboutSection

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../../animation";
 import { About, Description, Hide, Image } from "../../styles";
 
 import home1 from "../../img/home1.png";
@@ -10,25 +11,25 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to made</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to made</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography videography ideas that you have. We are
           pro with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </About>
   );
